1430e5dd8d893e03c0212b7ba4a7e9da78b07c0b
shafiquerajperbbaf23-cyber <shafiquerajper.bbaf23@iba-suk.edu.pk>
2026-04-29 18:18:43 +0500

    Update layout.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -13,7 +13,7 @@ export default async function AdminLayout({
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    redirect("/admin/login");
+    redirect("/admin-login");
   }
 
   return (
